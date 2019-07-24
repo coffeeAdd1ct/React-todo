24ff87fa0f33f06ec64442d454daddf03e065b0f
Kevin Billeaud <billeaudk@gmail.com>
2019-07-24 16:32:57 -0600

    add_item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import TodoList from './TodoList.js';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.sate = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+  handleInput = e => {
+    console.log('Hello Input')
+  }
+  addItem = () => {
+    console.log('Hello Add Item')
+  }
+  render() {
+    return (
+      <div className="App">
+        <TodoList addItem={this.addItem} />
+      </div>
+    )
+  }
 }
 
 export default App;
