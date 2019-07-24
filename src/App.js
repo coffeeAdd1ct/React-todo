a7a8c62bc09a5d17597d18c50e4e5c33d69b5d35
Kevin Billeaud <billeaudk@gmail.com>
2019-07-24 16:12:06 -0600

    init

--- src/App.js
+++ src/App.js
@@ -5,22 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
+// https://medium.com/@kris101/create-a-simple-todo-app-in-react-72d9341a7e6c
